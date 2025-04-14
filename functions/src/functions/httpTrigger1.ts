e98e6736709a30363d3f091b8d59d602ea07f472
eric <eric@enebelung.de>
2025-04-14 18:56:58 +0200

    test push to trigger auto deploy to az fun

--- functions/src/functions/httpTrigger1.ts
+++ functions/src/functions/httpTrigger1.ts
@@ -2,9 +2,7 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 
 export async function httpTrigger1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
-
     const name = request.query.get('name') || await request.text() || 'world';
-
     return { body: `Hello, ${name}!` };
 };
 
