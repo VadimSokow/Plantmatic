bd8665c76b044393d0e77ea0ef1d7dfac497c287
eric <eric@enebelung.de>
2025-04-16 17:07:46 +0200

    fix send message

--- functions/src/functions/c2d-test.ts
+++ functions/src/functions/c2d-test.ts
@@ -1,5 +1,6 @@
 import {app, HttpRequest, HttpResponseInit, InvocationContext} from "@azure/functions";
 import {Client} from "azure-iothub";
+import {Message} from "azure-iothub/dist/common-core/message";
 
 const iotHubConnectionString = process.env.IoTHub_Service_ConnectionString;
 
@@ -18,9 +19,12 @@ export async function c2dTest(request: HttpRequest, context: InvocationContext):
         const body: RequestBody = await request.json() as RequestBody;
         context.log(body);
         const client = Client.fromConnectionString(iotHubConnectionString);
+        await client.open();
         await client.send(body.deviceId, JSON.stringify(body.message));
+        await client.close();
     } catch (e) {
         context.error("Error:", e);
+        return { status: 500 }
     }
 
     return { status: 200 };
