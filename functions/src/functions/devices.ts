7360501a5a07befe1ae42f9359f42e36d87c8f33
Inno427 <innovativernmae9@gmail.com>
2025-05-20 14:38:23 +0200

    Added user.ts with getUser() Method and connetected it within getDevices

--- functions/src/functions/devices.ts
+++ functions/src/functions/devices.ts
@@ -1,5 +1,7 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions"
 import { CosmosClient } from "@azure/cosmos"
+import { getUser } from "./user";
+
 
 const cosmosEndpoint = process.env.CosmosDBEndpoint;
 const cosmosKey = process.env.CosmosDBKey;
@@ -15,6 +17,18 @@ export async function getDevices(request: HttpRequest, context: InvocationContex
         }
     }
 
+    const header = request.headers.get("authorization")
+            const user = getUser(header)
+    
+            if (!user){
+                console.error("Benutzer nicht da")
+                return{
+                    status: 401,
+                    body: "Das hier sollte eigentlich gar nciht passieren",
+                }
+            }
+    
+
 
     try {
         const client = new CosmosClient({
@@ -26,7 +40,12 @@ export async function getDevices(request: HttpRequest, context: InvocationContex
         const database = client.database(dbName)      
         const container = database.container(conName)         
 
-        const query = "SELECT * FROM c where c.owner = 'u38079@hs-harz.de'"
+        const query = {
+            query : "SELECT * FROM c where c.owner = @owner",
+            parameters: [
+                {name : "owner", value: user }
+            ]
+        }
         const { resources: items } = await container.items.query(query).fetchAll()
 
         return {
