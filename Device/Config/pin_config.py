ad79386d7d2d2a9ec5fbe60f91d83d0740b20adc
Alina <u38317@hs-harz.de>
2025-05-21 10:49:40 +0200

    Added Mqtt and Config Folder with Toml Files and Scripts. Connection_String and Path to ConfigFiles .toml have to be added

---
+++ Device/Config/pin_config.py
@@ -0,0 +1,38 @@
+import board
+import adafruit_ads1x15.ads1115 as ADS
+
+
+# --- Pin Konfiguration (BCM Nummerierung) ---
+DHT11_PIN = board.D4
+SOIL_MOISTURE_ADC_CHANNEL = ADS.P0  # A0
+#todo weitere Sensoren Konfigurieren
+# LIGHT_SENSOR_ADC_CHANNEL = 1  # A1
+
+# Pins für Aktoren
+RELAY_PIN_PUMP = board.D17
+LED_PIN_RED = board.D27
+LED_PIN_YELLOW = board.D22
+LED_PIN_GREEN = board.D5
+
+# --- Spannungen Bodenfeuchte Min / Max ---
+SOIL_MOISTURE_VOLTAGE_DRY: float = 2.21     #Spannung an der Luft
+SOIL_MOISTURE_VOLTAGE_WET: float = 0.935    #Spannung im Wasser
+
+# --- Logik-Schwellwerte ---
+# Bodenfeuchte (Beispielwerte in %) todo aus cloud config laden
+SOIL_MOISTURE_THRESHOLD_LOW = 30.0
+SOIL_MOISTURE_THRESHOLD_OK_LOW = 45.0
+SOIL_MOISTURE_THRESHOLD_OK_HIGH = 70.0
+
+# --- Zeit-Einstellungen ---
+SENSOR_READ_INTERVAL_SECONDS: int = 10       # Wie oft Sensoren lesen
+PUMP_ON_DURATION_SECONDS: float = 3.0        # Wie lange pumpen
+PUMP_COOLDOWN_SECONDS: int = 60 * 10     # Mindestpause zwischen Pumpvorgängen (10 Min)
+
+# --- Betriebsmodus ---
+USE_MOCK_HARDWARE: bool = False
+
+
+
+def get_pin_config(plant_config_toml):
+    print("Ich bin in Pin Config")
---
+++ Device/Main_Only_Sensor.py
@@ -0,0 +1,90 @@
+import time
+import config
+import RPi.GPIO as GPIO
+
+from Pflanzomat.interfaces.actuator_interface import LedColor
+from Pflanzomat.hardware.sensors.dht11_sensor import DHT11Sensor
+from Pflanzomat.hardware.sensors.ads1115_soil_moisture_sensor import ADS1115SoilSensor
+from Pflanzomat.hardware.actuators.pump import Pump
+from Pflanzomat.hardware.sensors.ads1115_light_sensor import ADS1115LightSensor
+from Pflanzomat.hardware.actuators.led_actuator import LedActuator
+
+# Temperatur- und Luftfeuchte Sensor initialisieren
+dht_sensor = DHT11Sensor(config.DHT11_PIN)
+# Bodenfeuchte Sensor initialisieren
+soil_sensor = ADS1115SoilSensor(
+    adc_channel=config.SOIL_MOISTURE_ADC_CHANNEL,
+    voltage_dry=config.SOIL_MOISTURE_VOLTAGE_DRY,
+    voltage_wet=config.SOIL_MOISTURE_VOLTAGE_WET
+)
+# Licht Sensor initialisieren
+light_sensor = ADS1115LightSensor(
+    config.LIGHT_SENSOR_ADC_CHANNEL,
+    config.LIGHT_SENSOR_VOLTAGE_DARK,
+    config.LIGHT_SENSOR_VOLTAGE_BRIGHT
+)
+# Pumpe initialisieren
+GPIO.setmode(GPIO.BCM) #für die Verwendung des Pins im setup der Pump.py
+pump = Pump(config.RELAY_PIN_PUMP.id)
+pump.setup()
+# LEDs initialisieren
+leds = LedActuator(config.LED_PIN_RED.id,config.LED_PIN_YELLOW.id,config.LED_PIN_GREEN.id)
+leds.setup()
+
+try:
+    while True:
+
+        # Temp und Luftfeuchte Auslesen
+        temp, hum = dht_sensor.read_combined()
+        if temp is not None and hum is not None:
+            print(f"DHT11: Temperatur={temp:.1f}°C, Luftfeuchtigkeit={hum:.1f}%")
+        else:
+            print("DHT11: Konnte nicht gelesen werden.")
+        #Bodenfeuchte Auslesen
+        moisture = soil_sensor.get_moisture_percent()
+        if moisture is not None:
+            print(f"Bodenfeuchte: {moisture:.1f}%")
+        else:
+            print("Bodenfeuchte: Konnte nicht gelesen werden.")
+
+        light = light_sensor.get_light_level_percent()
+        print(f"Lichtintensität: {light:.1f}%")
+
+
+        # Sicherheitsabschaltung, wenn Sensorwert fehlt
+        if moisture is None:
+            if pump.is_on:
+                print(
+                    "WARNUNG: Bodenfeuchte nicht lesbar, schalte Pumpe sicherheitshalber AUS!")
+                pump.pump_off()
+
+        # Pumpe aktivieren, wenn Bodenfeuchte über 60% (TODO Testweise)
+        if pump is None:
+            print("Fehler beim Initialiseren der Pumpe.")
+            break
+        if moisture is None:
+            print("Fehler beim Initialisieren des Bodenfeuchtesensors.")
+            break
+        if moisture > 60:
+            pump.pump_on()
+            print("Pumpe eingeschaltet.")
+            leds.set_status_color(LedColor.RED)
+        else:
+            pump.pump_off()
+            print("Pumpe ausgeschaltet.")
+            leds.set_status_color(LedColor.GREEN)
+
+        time.sleep(3)
+
+except KeyboardInterrupt:
+    print("\nTest gestoppt.")
+finally:
+    print("\nRäume auf...")
+    if 'dht_sensor' in locals():
+        dht_sensor.cleanup()
+    if 'soil_sensor' in locals():
+        soil_sensor.cleanup()
+    if 'pump' in locals():
+        pump.cleanup()
+    GPIO.cleanup()
+    print("Aufgeräumt.")
