4fed2ea356e5b0bbc1e65b94a7314d8ef938c783
eric <eric@enebelung.de>
2025-06-16 20:51:53 +0200

    restructure create plant to prevent plant invalid plant creation

--- functions/src/functions/createPlant.ts
+++ functions/src/functions/createPlant.ts
@@ -5,8 +5,6 @@ import {handleExtractUserEmail} from "../helper/auth";
 
 
 export async function createPlant(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-
-
     let email = handleExtractUserEmail(request)
     if (typeof email !== 'string') {
         return email;
@@ -19,57 +17,33 @@ export async function createPlant(request: HttpRequest, context: InvocationConte
             return {status: 500, body: "Database not available"}
         }
 
-
+        // some cosmos prepare
         const plantConName = "plants"
         const deviceConName = "devices"
         const deviceContainer = cosmos.db.container(deviceConName)
         const plantContainer = cosmos.db.container(plantConName)
 
-
+        // collect body
         const body: any = await request.json()
 
         //für Pflanze
         const name = body.name
         const latName = body.latName
 
-        //für Jeraät
-
+        // extract device data
         const deviceId = body.deviceId
         const slotNumber = body.slotNumber
 
         console.log("Daten fuer Device: ", deviceId, slotNumber)
 
+        // check if all device data is present
         if (!name || !latName || !deviceId || !slotNumber) {
             return {
                 status: 400
             }
-
         }
 
-
-        const userId = email
-        const newPlant = {
-            userId,
-            name,
-            latName,
-        }
-
-
-        const {resource} = await plantContainer.items.create(newPlant)
-
-        if (!resource){
-            return {
-                status: 500,
-                body: "Datenbank hat Pflanze nicht erstellt"
-            }
-        }
-
-        console.log(resource)
-
-        const plantId = resource.id
-
-        console.log("ID der Pflanze: ", resource.id)
-
+        // check if the slot in the device is empty
         const deviceQuery = {
             query: "SELECT * FROM c WHERE c.id = @id",
             parameters: [
@@ -87,8 +61,8 @@ export async function createPlant(request: HttpRequest, context: InvocationConte
         }
         const device = devices[0]
 
+        // get the right slot definition of the device
         console.log("Gerät: ", device)
-
         const ps = device.plantSlots as Array<{ plantId: string | null, slotNumber: number }> | undefined
         console.log("Slots: ", ps)
         if (!ps) {
@@ -98,14 +72,16 @@ export async function createPlant(request: HttpRequest, context: InvocationConte
             }
         }
         const slotIndex = ps.findIndex(slot => slot.slotNumber == slotNumber)
+        console.log(`slot ${slotNumber} has ${slotIndex} as index`)
         if (slotIndex < 0) {
             return {
                 status: 404,
                 body: "Slotnummer mit der übergebenen Nummer nicht gefunden"
             }
         }
-
         const slotDefinition = ps[slotIndex]
+        console.log("slotDefinition: ", slotDefinition)
+        // check if a plant is already present, when yes -> error
         if (slotDefinition.plantId != null) {
             return {
                 status: 409,
@@ -113,11 +89,31 @@ export async function createPlant(request: HttpRequest, context: InvocationConte
             }
         }
 
+        // build a new plant Entry
+        const userId = email
+        const newPlant = {
+            userId,
+            name,
+            latName,
+        }
+
+        // insert the plant into the DB
+        const {resource} = await plantContainer.items.create(newPlant)
+        if (!resource){
+            return {
+                status: 500,
+                body: "Datenbank hat Pflanze nicht erstellt"
+            }
+        }
+        console.log(resource)
+        const plantId = resource.id
+        console.log("ID der Pflanze: ", resource.id)
+
+        // update the plantId and update the DB entry of the device
         slotDefinition.plantId = plantId
         device.plantSlots[slotIndex] = slotDefinition
-
+        console.log("new Device def: ", device)
         const {resource: updatedDevice} = await deviceContainer.item(device.id, device.id).replace(device)
-
         console.log("Device aktualisiert: ", updatedDevice)
 
         return {
